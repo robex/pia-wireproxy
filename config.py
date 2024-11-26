395337072801c1cbad3213f4dd5d21156cce088f
robex <23484075+robex@users.noreply.github.com>
2024-11-26 21:48:43 +0100

    move config parameters to compose

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
-PIA_USER = ""
-PIA_PASS = ""
-PIA_LOCS = ["ES", "FR", "DE"]
-PIA_PORT_START = 26000
-SOCKS_USER = ""
-SOCKS_PASS = ""
+import os
+
+PIA_USER = os.environ["PIA_USER"]
+PIA_PASS = os.environ["PIA_PASS"]
+PIA_LOCS = os.environ["PIA_LOCS"].split(",")
+PIA_PORT_START = int(os.environ["PIA_PORT_START"])
+SOCKS_USER = os.environ["SOCKS_USER"]
+SOCKS_PASS = os.environ["SOCKS_PASS"]
